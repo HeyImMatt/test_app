896e78c8f2fbbeaabc670e32fbf2696770528a83
HeyImMatt <mrmcfarland@gmail.com>
2016-03-28 22:39:39 -0700

    Navbar in partial. Rendering on app.html.erb page. Add method to app_helper.rb so navbar is aware of the active page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+#Found the following to make the navbar recognize which page is active:
+#http://stackoverflow.com/questions/17481812/dynamically-add-active-class-to-bootstrap-li-in-rails
+
+  def is_active?(link_path)
+    current_page?(link_path) ? "active" : ""
+  end
+
 end
