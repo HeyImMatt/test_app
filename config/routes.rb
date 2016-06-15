f4bb89cc078e993718e0f9f9b9ce68af419e4dea
HeyImMatt <mrmcfarland@gmail.com>
2016-06-14 17:38:02 -0700

    Added login and logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,17 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-root "welcome#home"
-get "about", to: "welcome#about"
+root 'welcome#home'
+get 'about', to: 'welcome#about'
 
 resources :articles
 
-get "signup", to: "users#new"
+get 'signup', to: 'users#new'
 resources :users, except: [:new]
+
+get 'login', to: 'sessions#new'
+post 'login', to: 'sessions#create'
+delete 'logout', to: 'sessions#destroy'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
